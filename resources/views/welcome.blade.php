9104698b21a1723554f53bc60b6a2f51d10b6ad1
devaandraf <andrafadila@gmail.com>
2020-01-28 15:08:08 +0700

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Laradinus comunity</title>
+</head>
+<body background="img/bg.jpg">
+  <font face="Roboto" color="#3b3b3b">
+    
+  
+
+  <table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" height="100%" background="">
+    <tboody>
+      <tr>
+        <td>
+          <table width="1140px" border="0"  align="center">
+            <tbody>
+              <!-- Header -->
+              <tr>
+                <td width="570px">
+                  <a href="">
+                    <img src="img/logo-200.png" height="100px">
+                  </a>
+                </td>
+                <td align="right">
+                  <b><a href="index.html" title="Home"><font face="Roboto" color="#fa6f49">Home</font></a></b> &nbsp;&nbsp;&nbsp;
+                  <b><a href="portofolio.html" title="Portofolio"><font face="Roboto" color="#3b3b3b">Portofolio</font></a></b> &nbsp;&nbsp;&nbsp;
+                  <b><a href="about.html" title="About Me"><font face="Roboto" color="#3b3b3b">About Us</font></a></b> &nbsp;&nbsp;&nbsp;
+                  <b><a href="blog.html" title="Blog"><font face="Roboto" color="#3b3b3b">Blog</font></a></b> &nbsp;&nbsp;&nbsp;
+                  <b><a href="contact.html" title="Contact Me"><font face="Roboto" color="#3b3b3b">Contact Us</font></a></b> &nbsp;&nbsp;&nbsp;
+                  <b><a href="login.html" title="Login"><font face="Roboto" color="#3b3b3b">Login</font></a></b> &nbsp;&nbsp;&nbsp;
+                </td>
+
+              </tr>
+              <tr>
+                <td height="30px"  colspan="2"></td>
+              </tr>
+              <!-- Content -->
+              <tr>
+                <td colspan="2">
+                <font size="30"><b>Welcome to Unidus<br> Laravel <font color="#fa6f49">Comunity.</font></b></font>
+                <br>
+                <br>
+                <p>
+                <b>========</b>
+                <br>
+                <br>
+                komunitas laravel Universitas Dian Nuswantoro, Bersama ciptakan teknologi untuk membangun negeri.
+                <br>
+                join bergabung bersama kami sekarang juga.
+                </p>
+                </td>
+              </tr>
+              
+              <tr>
+                <td height="30px"  colspan="2"></td>
+              </tr>
+              
+              <tr>
+                <td colspan="2">
+                <img src="img/content.jpg" width="100%">
+                </td>
+              </tr>
+
+              <tr>
+                <td height="30px"  colspan="2"></td>
+              </tr>
+              <!-- Footer -->
+              <tr>
+                <td>
+                  <img src="img/logo-200-black.png" width="35px"> Copyright &copy; 2017 Fiqi Arifianto</td>
+                </td>
+                <td>
+                  
+                </td>
+              </tr>
+
+
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tboody>
+  </table>
+
+
+
+
+
+  </font>
+</body>
+</html>
